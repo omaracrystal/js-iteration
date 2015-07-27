133ce39d80d91203f6cc2deba69d01bb64c5fac7
omaracrystal <omaracrystal@gmail.com>
2015-07-27 00:21:20 -0600

    created a function called nameObjectArray that could then be used within the filler functions so that once event listeners are called will print a specific list into the answer box

--- domNames.js
+++ domNames.js
@@ -1,71 +1,53 @@
 //////////////////////////////////names.html//////////////////////////////////
 
 
-///////grab all buttons
+////grab all buttons
 var printAllNames = document.getElementById("all-names");
 var firstNames = document.getElementById("first-names");
 var lastNames = document.getElementById("last-names");
 var nameLengths = document.getElementById("names-with-lengths")
 
 
-/////get ul "answersUl"
+
+////grab ul "answersUl"
 var answerList = document.getElementById("answersUl")
-
-
-/////arrays that will be passed through the listAnswers(); function once the click event happens/////
-//get the string of names and split it into an array
-var nameString = document.getElementsByTagName("textarea")[0].value;
-var nameArray = nameString.split("\n");
-//take the array of names and split it into first names
-// var firstNameArray =
-// // //take the array of names and split it into last names
-// var lastNameArray =
-// // //take the array of names and output the total length of their names
-// var nameLengthArray =
-
-// for (i in nameArray) {
-//   create objects of all i;
-//   split the new objects with key words first and last name;
-//   we can then pull first.value and last.value;
-// }
-
-
-
-/////////functions that append li elements to "answerUl" textarea and print innerHTML to li elements once event listener is called
-var nameFiller = function() {
-  for (var i = 0; i < 20; i++) {
+//append "li" elements to "answersUL"
+  for (var i = 0; i < objectList.length; i++) {
     var li = document.createElement("li");
     answerList.appendChild(li);
-    answerList.children[i].innerHTML = nameArray[i];
+    };
+
+
+
+////functions that print innerHTML to li elements once event listener is called
+var nameFiller = function() {
+  for (var i = 0; i < objectList.length; i++) {
+    answerList.children[i].innerHTML = nameArray[i]
   }
 }
 
 var firstNameFiller = function() {
-  for (var i = 0; i < 20; i++) {
-    var li = document.createElement("li");
-    answerList.appendChild(li);
-    answerList.children[i].innerHTML = firstNameArray[i];
+  for (var i = 0; i < objectList.length; i++) {
+    answerList.children[i].innerHTML = objectList[i].first;
   }
 }
 
 var lastNameFiller = function() {
-  for (var i = 0; i < 20; i++) {
-    var li = document.createElement("li");
-    answerList.appendChild(li);
-    answerList.children[i].innerHTML = lastNameArray[i];
+  for (var i = 0; i < objectList.length; i++) {
+    answerList.children[i].innerHTML = objectList[i].last;
   }
 }
 
 var nameLengthFiller = function() {
-  for (var i = 0; i < 20; i++) {
-    var li = document.createElement("li");
-    answerList.appendChild(li);
-    answerList.children[i].innerHTML = nameLengthArray[i];
+  for (var i = 0; i < objectList.length; i++) {
+    answerList.children[i].innerHTML = nameArray[i].length;
   }
 }
 
-////////add event listeners to all buttons
+
+
+////add event listeners to all buttons
 printAllNames.addEventListener("click", nameFiller)
-// firstNames.addEventListener("click", listAnswers(firstNameArray))
-// lastNames.addEventListener("click", listAnswers(lastNameArray))
-// nameLengths.addEventListener("click", listAnswers(nameLengthArray))
+firstNames.addEventListener("click", firstNameFiller)
+lastNames.addEventListener("click", lastNameFiller)
+nameLengths.addEventListener("click", nameLengthFiller)
--- logicNames.js
+++ logicNames.js
@@ -1,2 +1,24 @@
 //////////////////////////////////names.html//////////////////////////////////
 
+////get the string of names and split it into an array
+var nameString = document.getElementsByTagName("textarea")[0].value;
+var nameArray = nameString.split("\n");
+
+
+
+////create a list of fullName objects with key, value pairs being first & last
+var nameObjectArray = function (nameArray) {
+    //holder for new object array
+    objectList = [];
+  for (var i=0; i < nameArray.length; i++) {
+    //trim excess spaces
+    var trimmed=nameArray[i].trim();
+    //split on spaces in between
+    var names = trimmed.split(" ");
+    //push object onto array
+    objectList.push({"first": names[0], "last": names[1]});
+  };
+  console.log(objectList);
+  }
+
+nameObjectArray(nameArray);
